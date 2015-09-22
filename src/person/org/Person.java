827c9871b338d78bde07ee0a98462775cf35e670
unknown <alvarosuarez.1453@gmail.com>
2015-09-22 09:08:59 +0100

    Created constructor with name, surname and birthday, and created getter

--- src/person/org/Person.java
+++ src/person/org/Person.java
@@ -1,13 +1,28 @@
 package person.org;
+import java.util.Date;
 
 public class Person {
 
     private final String name;
-    private int edad;
+    private final String surname;
+    private final Date birthday;
 
-    public Person(String name, int edad) {
+    public Person(String name, String surname, int edad, Date birthday) {
         this.name = name;
-        this.edad = edad;
+        this.surname = surname;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
     }
 
 }
