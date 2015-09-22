41f0e32282408383717712e4ac3f500818a63e9a
unknown <alvarosuarez.1453@gmail.com>
2015-09-22 08:59:44 +0100

    First commit, class Person created.

---
+++ src/person/org/Person.java
@@ -0,0 +1,13 @@
+package person.org;
+
+public class Person {
+
+    private final String name;
+    private int edad;
+
+    public Person(String name, int edad) {
+        this.name = name;
+        this.edad = edad;
+    }
+
+}
